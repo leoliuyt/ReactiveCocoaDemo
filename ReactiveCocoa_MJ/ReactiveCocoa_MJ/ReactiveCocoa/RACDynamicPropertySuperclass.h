ff37a54c06bcf37920c8ed4fe549cdd57d6340d7
leoliuyt <leoliuyt@sina.cn>
2015-10-09 13:02:30 +0800

    添加ReactiveCocoa

---
+++ ReactiveCocoa_MJ/ReactiveCocoa_MJ/ReactiveCocoa/RACDynamicPropertySuperclass.h
@@ -0,0 +1,9 @@
+//  Copyright (c) 2015 GitHub. All rights reserved.
+
+// 拖入了C++ 文件 在此处@import Foundation报错; 故改为 #import <Foundation/Foundation.h>
+//@import Foundation;
+#import <Foundation/Foundation.h>
+/// This superclass is an implementation detail of DynamicProperty. Do
+/// not use it.
+@interface RACDynamicPropertySuperclass : NSObject
+@end
